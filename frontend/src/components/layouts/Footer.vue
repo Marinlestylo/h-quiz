581c1b6542eea28da0b2dc1ffc58974f15db7af4
Marinlestylo <chil.tfm@gmail.com>
2023-04-24 23:02:08 +0200

    refactor: remove useless files from base project and create components for navbar and footer

---
+++ frontend/src/components/layouts/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer>
+      <div class="w-full bg-gray-900 p-4 flex justify-end">
+        <div>
+          <img src="@/assets/images/logo-vd.svg" alt="Canton de Vaud" class="h-16 inline-block mr-4" />
+        </div>
+        <div>
+          <img src="@/assets/images/logo-hes-so.svg" alt="HES-SO" class="h-16 inline-block" />
+        </div>
+      </div>
+    </footer>
+</template>
---
+++ frontend/src/components/layouts/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="w-full bg-gray-900 p-4">
+        <nav class="text-white flex items-center">
+            <a href="https://heig-vd.ch" target="_blank">
+                <img src="@/assets/images/logo-heig-vd.svg" alt="Logo" class="h-12 inline-block" />
+            </a>
+            <div>
+                <img src="@/assets/images/quiz.svg" alt="Logo" class="h-12 inline-block" />
+            </div>
+            <RouterLink to="/" class="ml-4 hover:text-gray-300">Home</RouterLink>
+            <RouterLink to="/about" class="ml-4 hover:text-gray-300">About</RouterLink>
+        </nav>
+    </header>
+</template>
