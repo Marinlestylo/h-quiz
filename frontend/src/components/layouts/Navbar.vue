479d49a4ab2ea1a2134a2364a7e4eaf558c7fbed
Marinlestylo <chil.tfm@gmail.com>
2023-06-20 22:10:40 +0200

    feat: changed nav + quizzes page

--- frontend/src/components/layouts/Navbar.vue
+++ frontend/src/components/layouts/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="w-full bg-gray-900 p-4">
         <nav class="text-white flex justify-between">
-            <div class="flex items-center">
+            <div class="flex">
                 <a href="https://heig-vd.ch" target="_blank">
                     <img src="@/assets/images/logo-heig-vd.svg" alt="Logo" class="h-12 inline-block" />
                 </a>
@@ -10,16 +10,14 @@
                         <img src="@/assets/images/quiz.svg" alt="Logo" class="h-12 inline-block" />
                     </div>
                 </RouterLink>
-                <RouterLink to="/quizzes" class="ml-4 hover:text-gray-300">Quizzes</RouterLink>
-                <RouterLink to="/sandbox" class="ml-4 hover:text-gray-300">Bac à sable</RouterLink>
-                <RouterLink to="/create-question" class="ml-4 hover:text-gray-300">Créer une Question</RouterLink>
-                <RouterLink to="/update-question" class="ml-4 hover:text-gray-300">Modifier une Question</RouterLink>
-                <RouterLink to="/create-quiz" class="ml-4 hover:text-gray-300">Créer un Quiz</RouterLink>
-                <RouterLink to="/update-quiz" class="ml-4 hover:text-gray-300">Modifier un Quiz</RouterLink>
-                <RouterLink to="/create-roster" class="ml-4 hover:text-gray-300">Créer un Roster</RouterLink>
-                <RouterLink to="/roster" class="ml-4 hover:text-gray-300">Roster</RouterLink>
             </div>
-            <div>
+            <div class="flex items-center">
+                <RouterLink to="/activities" class="ml-4 hover:text-gray-300">Activités</RouterLink>
+                <RouterLink to="/quizzes" class="ml-4 hover:text-gray-300">Quizzes</RouterLink>
+                <RouterLink to="/questions" class="ml-4 hover:text-gray-300">Questions</RouterLink>
+                <RouterLink to="/rosters" class="ml-4 hover:text-gray-300">Rosters</RouterLink>
+            </div>
+            <div class="flex items-center">
                 <Connection />
             </div>
         </nav>
--- frontend/src/views/QuizzesView.vue
+++ frontend/src/views/QuizzesView.vue
@@ -2,11 +2,19 @@
     <div v-if="!isLoading">
         <ErrorMessage :status="status" />
     </div>
-    <div class="text-3xl mb-6">
-        Tous les quiz
+    <div v-show="!isLoading" class="flex items-center mb-6 justify-between">
+        <div class="text-3xl">
+            Tous les quiz
+        </div>
+        <div>
+            <RouterLink to="/create-quiz"
+                class="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded ml-48">
+                Créer un quiz
+            </RouterLink>
+        </div>
     </div>
-    <div
-        class="max-w-5xl rounded overflow-hidden shadow-lg border border-gray-400 bg-white rounded-b p-4 my-6">
+    <div v-show="!isLoading"
+        class="max-w-5xl rounded overflow-hidden shadow-lg border border-gray-400 bg-white rounded-b px-4 my-6">
         <div class="flex flex-col">
             <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
@@ -25,17 +33,25 @@
                                 <tr v-for="item in listItems.quizzes"
                                     class="border-b transition duration-300 ease-in-out hover:bg-neutral-200">
                                     <td class="whitespace-nowrap px-6 py-4 font-medium">{{ item.id }}</td>
-                                    <td class="whitespace-nowrap px-6 py-4">{{ item.name }}</td>
-                                    <td class="whitespace-nowrap px-6 py-4">{{ item.question_count }}</td>
-                                    <td class="whitespace-nowrap px-6 py-4">{{ item.owner.firstname + " " +
+                                    <td class="whitespace-nowrap px-6 py-4 font-medium">{{ item.name }}</td>
+                                    <td class="whitespace-nowrap px-6 py-4 font-medium">{{ item.question_count }}</td>
+                                    <td class="whitespace-nowrap px-6 py-4 font-medium">{{ item.owner.firstname + " " +
                                         item.owner.lastname }}
                                     </td>
-                                    <td class="whitespace-nowrap px-6 py-4"><svg xmlns="http://www.w3.org/2000/svg"
+                                    <td class="whitespace-nowrap px-6 py-4 flex"><svg xmlns="http://www.w3.org/2000/svg"
                                             fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                            class="w-10 h-10">
+                                            class="w-10 h-10 hover:cursor-pointer">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                         </svg>
+                                        <RouterLink to="/update-quiz">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor"
+                                                class="w-9 h-9 hover:cursor-pointer ml-2">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                            </svg>
+                                        </RouterLink>
                                     </td>
                                 </tr>
                             </tbody>
